c16ac4480f117e34280fa6a494ec789cc32aec53
iptablecommandcode <iptablecommandcode@gmail.com>
2024-07-26 17:35:03 +0900

    java 소스 완성 및 자동 변경할 text 파일 추가

---
+++ src/quotation.java
@@ -0,0 +1,36 @@
+package src;
+
+import com.sun.tools.javac.Main;
+
+import java.io.*;
+
+public class quotation {
+    public static void main(String[] args) {
+        //파일 위치 가져오기
+        String jarDir = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        String filePath = jarDir + File.separator + "changeText.txt";
+
+        //changeText 파일 읽기
+        StringBuffer result = new StringBuffer();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            //파일에서 한 줄씩 읽어오기
+            while ((line = reader.readLine()) != null) {
+                // 각 줄의 앞뒤 공백 제거 후 작은 따옴표 추가하여 결과에 추가
+                result.append("'").append(line.trim()).append("'\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //마지막 ,\n 제거
+        result.delete(result.length() - 2, result.length());
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(result.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
